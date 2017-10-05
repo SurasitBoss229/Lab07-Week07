ca226e7f0683fc97c81172eb56dfc9cb6f0cf8f3
Surasit <surasitboss229@gmail.com>
2017-10-05 13:26:49 +0700

    W07P02

--- Lab07-Week07/Lab07-Week07/Program.cs
+++ Lab07-Week07/Lab07-Week07/Program.cs
@@ -10,7 +10,35 @@ namespace Lab07_Week07
     {
         static void Main(string[] args)
         {
-
+            Student su = new Student();
+            su.Name = "Student Name";
+            su.ID = "12345678";
+            su.GPA = 7.5f;
+            Console.WriteLine("Student name : " + su.Name);
+            Console.WriteLine("Student ID   : " + su.ID);
+            Console.WriteLine("Student GPA  : " + su.GPA);
+            Console.ReadLine();
+        }
+        class Student
+        {
+            private string name;
+            private string id;
+            private float gpa;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public string ID
+            {
+                get { return id; }
+                set { id = value; }
+            }
+            public float GPA
+            {
+                get { return gpa; }
+                set { gpa = value; }
+            }
         }
     }
 }
