3100b6363ba22420f602dbc252d7c5141c3b9428
Surasit <surasitboss229@gmail.com>
2017-10-05 15:10:32 +0700

    W07P09

--- Lab07-Week07/Lab07-Week07/Program.cs
+++ Lab07-Week07/Lab07-Week07/Program.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace Lab07_Week07
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
+            Console.ReadKey();
+        }
+    }
+
     class Person
     {
         public virtual void SayHi()
